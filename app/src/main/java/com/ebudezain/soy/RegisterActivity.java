1b5047bd75744421f10e1cdceb32bf4740a9490b
TTH <thanhhung.dev@gmail.com>
2021-05-20 18:00:05 +0900

    goto login

--- app/src/main/java/com/ebudezain/soy/RegisterActivity.java
+++ app/src/main/java/com/ebudezain/soy/RegisterActivity.java
@@ -2,13 +2,27 @@ package com.ebudezain.soy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btnGotoLogin = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnGotoLogin = (Button) findViewById(R.id.btnGotoLogin);
+        btnGotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /// qua trang view list
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
